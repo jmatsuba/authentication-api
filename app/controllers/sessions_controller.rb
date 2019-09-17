d4883fbc4d65f6a61f3b1083b28703ba03c4b1ef
James Matsuba <james.matsuba@gmail.com>
2019-09-17 07:45:45 -0700

    basic login working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,20 @@
 class SessionsController < ApplicationController
   def create
+    result = AuthenticateUser.call(username: params[:session][:username], password: params[:session][:password])
+
+    if result.success?
+      render json: { auth_token: result.token }
+    else
+      render json: { error: result.message }, status: :unauthorized
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:username, :password)
   end
 end
---
+++ app/services/authenticate_user.rb
@@ -0,0 +1,22 @@
+class AuthenticateUser
+  include Interactor
+
+  def call
+    user = authentication
+
+    if user
+      context.user = user
+      context.token = JsonWebToken.encode(user_id: user.id)
+    else
+      context.fail!(message: "invalid credentials")
+    end
+  end
+
+  private
+
+  def authentication
+    user = User.find_by_username(context.username)
+    correct_password = user ? user.authenticate(context.password) : false
+    return user if user && correct_password
+  end
+end
