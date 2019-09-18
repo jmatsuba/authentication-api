8fc8b25720ab249b91a67dd038c0ed444dc8e4ad
James Matsuba <james.matsuba@gmail.com>
2019-09-18 09:11:39 -0700

    a more friendly response for created user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      render status: :created
+      render json: { status: 'user created successfully' }, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
